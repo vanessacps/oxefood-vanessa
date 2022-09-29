3cdd4939d952594e8567a7101fce50e4dae97461
Felipe Vieira da Rocha <felipevieiradarocha@hotmail.com>
2022-09-29 09:15:20 -0300

    enviando os primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefoodvanessa/OxefoodVanessaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodvanessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodVanessaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodVanessaApplication.class, args);
+	}
+
+}
